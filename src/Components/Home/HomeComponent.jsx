a797fbc928ec7b0697ec5be71a0765e3e8e8ce69
Rafin Hasan <168470414+Rafin-Hasan@users.noreply.github.com>
2024-09-02 09:28:59 +0600

    modified:   package-lock.json 	modified:   package.json 	modified:   src/Components/Home/HomeComponent.jsx 	modified:   src/Components/Navber/NavberComponent.css 	modified:   src/Components/Navber/NavberComponent.jsx

--- src/Components/Home/HomeComponent.jsx
+++ src/Components/Home/HomeComponent.jsx
@@ -1,22 +1,66 @@
-import React from "react";
+import React, { useEffect } from "react";
+import AOS from "aos";
+import "aos/dist/aos.css";
 import "./HomeComponent.css";
 
 const HomeComponent = () => {
+  useEffect(() => {
+    AOS.init({
+      duration: 1200, // Animation duration (in ms)
+      once: true, // Animation happens only once
+    });
+  }, []);
+
   return (
     <>
-      <section className=" bg">
-        <div className="flex container">
+      <section className="bg">
+        <div className="flex gap-[119px] container">
           <div>
-            <div className="mt-[166px]">
-              <h1 className="w-[620px] inter font-extrabold text-[50px] pl-[145px]">
-                Hi, I’m Rafin Hasan!
+            <div className="mt-[166px]" data-aos="fade-up">
+              <h1
+                className="w-[620px] inter text-[#130F49] font-extrabold text-[50px] pl-[145px]"
+                data-aos="fade-right"
+              >
+                Hi, I’m <span className="text-[#A5A6FF]">Rafin Hasan</span>!
               </h1>
+              <h2
+                className="w-[620px] savilla text-[#130F49] font-bold text-[50px] pl-[145px]"
+                data-aos="fade-left"
+              >
+                Creative web developer based on React js.
+              </h2>
+              <p
+                className="w-[620px] savilla text-[#130F49] font-normal text-[20px] pt-3 pl-[145px]"
+                data-aos="fade-up"
+              >
+                I'm a React based professional frontend developer.
+              </p>
+            </div>
+            <div className="pt-[56px] pl-[145px]" data-aos="zoom-in">
+              <button className="bg-[#130F49] text-white font-semibold py-2 px-6 rounded-full hover:bg-white hover:text-[#130F49] border-2 border-[#130F49] transition duration-300 ease-in-out">
+                Get a project
+              </button>
+              <button className="bg-transparent text-[#130F49] font-semibold py-2 px-6 rounded-full hover:bg-[#130F49] hover:text-white border-2 border-[#130F49] transition duration-300 ease-in-out ml-4">
+                Let's talk
+              </button>
+            </div>
+            <div className="flex pl-[130px] pt-[80px]" data-aos="fade-up">
+              <div className="w-[1px] h-[92px]"></div>
+              <div className="flex items-center bg-[#F4FBF6] p-4">
+                <div className="border-l-4 border-[#f58b00] h-full mr-4"></div>
+                <div className="text-[#130F49] inter leading-3">
+                  <p className="text-lg font-semibold">+8801778566730</p>
+                  <p className="text-base">rafinhasan.web@gmail.com</p>
+                  <p className="text-base">Dhaka, Bangladesh</p>
+                </div>
+              </div>
             </div>
           </div>
           <img
-            className="pt-[100px] pr-[100px] w-[545px] h-[621px] "
+            className="pt-[100px] pr-[100px] w-[545px] h-[621px]"
             src="./img/banner.png"
             alt="banner"
+            data-aos="fade-left"
           />
         </div>
       </section>
--- src/Components/Navber/NavberComponent.jsx
+++ src/Components/Navber/NavberComponent.jsx
@@ -1,33 +1,52 @@
-import React from "react";
+import React, { useEffect } from "react";
+import AOS from "aos";
+import "aos/dist/aos.css";
 import "./NavberComponent.css";
 import { NavLink } from "react-router-dom";
-import { FaGithub } from "react-icons/fa6";
-import { FaFacebook } from "react-icons/fa6";
-import { FaLinkedin } from "react-icons/fa6";
-import styled from "styled-components";
 import Card from "./SocialMedia";
 
 const NavberComponent = () => {
+  useEffect(() => {
+    AOS.init({
+      duration: 1200,
+      once: true, // Animation happens only once as you scroll down
+    });
+  }, []);
+
   return (
     <>
-      <nav className="bg-white">
-        <div className="flex flex-col gap-[93px] pt-[50px] w-[370px]">
+      <nav className=" ">
+        <div
+          className="flex bg-white bg flex-col gap-[93px] pt-[50px] w-[370px] rounded-[20px] shadow-lg"
+          data-aos="fade-up"
+        >
           {/* =========== Logo and Name part starts ============= */}
-          <div>
-            <img className="px-[125px]" src="./img/logo.png" alt="logo" />
+          <div data-aos="flip-left">
+            <img
+              className="px-[125px]"
+              src="./img/logo.png"
+              alt="logo"
+              data-aos="zoom-in-down"
+            />
             <div className="relative">
-              <h4 className="satisfy font-normal text-[60px] text-[#d8d8df] pl-[50px] absolute nam">
+              <h4
+                className="satisfy font-normal text-[60px] text-[#d8d8df] pl-[50px] absolute nam"
+                data-aos="fade-left"
+              >
                 Rafin Hasan
               </h4>
-              <h5 className="inter text-center font-bold text-[30px] pt-[22px] nam2">
+              <h5
+                className="inter text-center font-bold text-[30px] pt-[22px] "
+                data-aos="fade-right"
+              >
                 Rafin Hasan
               </h5>
             </div>
           </div>
           {/* =========== Logo and Name part ends ============= */}
 
-          {/* =========== menu part starts ============= */}
-          <div>
+          {/* =========== Menu part starts ============= */}
+          <div data-aos="fade-up">
             <ul className="flex flex-col gap-[20px] inter font-semibold">
               <li>
                 <NavLink
@@ -36,9 +55,10 @@ const NavberComponent = () => {
                     `text-[#333333] ${
                       isActive
                         ? "hover:text-[#d8d8df] text-[#A5A6FF] font-bold"
-                        : "hover:underline-offset-1"
+                        : "hover:text-[#d8d8df]"
                     } flex justify-center`
                   }
+                  data-aos="fade-right"
                 >
                   Home
                 </NavLink>
@@ -50,9 +70,10 @@ const NavberComponent = () => {
                     `text-[#333333] ${
                       isActive
                         ? "hover:text-[#d8d8df] text-[#A5A6FF]"
-                        : "hover:underline-offset-auto  "
+                        : "hover:text-[#d8d8df]  "
                     } flex justify-center`
                   }
+                  data-aos="fade-left"
                 >
                   About
                 </NavLink>
@@ -64,9 +85,10 @@ const NavberComponent = () => {
                     `text-[#333333] ${
                       isActive
                         ? "hover:text-[#d8d8df] text-[#A5A6FF]"
-                        : "hover:underline-offset-1"
+                        : "hover:text-[#d8d8df]"
                     } flex justify-center`
                   }
+                  data-aos="flip-up"
                 >
                   Services
                 </NavLink>
@@ -78,9 +100,10 @@ const NavberComponent = () => {
                     `text-[#333333] ${
                       isActive
                         ? "hover:text-[#d8d8df] text-[#A5A6FF]"
-                        : "hover:underline-offset-1 "
+                        : "hover:text-[#d8d8df]"
                     } flex justify-center`
                   }
+                  data-aos="flip-down"
                 >
                   Works
                 </NavLink>
@@ -92,25 +115,29 @@ const NavberComponent = () => {
                     `text-[#333333] ${
                       isActive
                         ? "hover:text-[#d8d8df] text-[#A5A6FF]"
-                        : "hover:underline-offset-1"
+                        : "hover:text-[#d8d8df]"
                     } flex justify-center`
                   }
+                  data-aos="zoom-in"
                 >
                   Contact
                 </NavLink>
               </li>
             </ul>
           </div>
-          {/* =========== Logo and Name part ends ============= */}
+          {/* =========== Menu part ends ============= */}
 
-          <div className="pt-[25px]">
-            <div className="flex justify-center gap-3 pb-6">
-              {/* ============ social media ============== */}
+          <div className="pt-[25px]" data-aos="fade-up">
+            <div className="flex justify-center gap-3 pb-6" data-aos="zoom-out">
+              {/* ============ Social Media ============== */}
               <Card />
-              {/* ============ social media ============== */}
+              {/* ============ Social Media ============== */}
             </div>
             <div>
-              <p className="inter w-[230px] text-center m-auto leading-6 font-medium text-[16px] mb-[20px]">
+              <p
+                className="inter w-[230px] text-center m-auto leading-6 font-medium text-[16px] mb-[20px]"
+                data-aos="fade-up"
+              >
                 Copyright ©2024 Rafin Hasan. All right reserved.
               </p>
             </div>
