db95dd98269c59ae85b7b546ee0a91eb9f6ecef8
Rafin Hasan <168470414+Rafin-Hasan@users.noreply.github.com>
2024-09-03 11:43:01 +0600

    modified:   public/img/banner.png 	new file:   src/Components/About/AboutComponent.css 	new file:   src/Components/About/AboutComponent.jsx 	modified:   src/Pages/AboutPage.jsx

--- src/Pages/AboutPage.jsx
+++ src/Pages/AboutPage.jsx
@@ -1,7 +1,12 @@
 import React from "react";
+import AboutComponent from "../Components/About/AboutComponent";
 
 const AboutPage = () => {
-  return <div>AboutPage</div>;
+  return (
+    <div>
+      <AboutComponent />
+    </div>
+  );
 };
 
 export default AboutPage;
