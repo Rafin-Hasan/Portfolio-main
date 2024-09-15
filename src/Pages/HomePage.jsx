4ccecd126f095e20d96af17516600455d668c7fc
Rafin Hasan <168470414+Rafin-Hasan@users.noreply.github.com>
2024-09-15 23:48:18 +0600

    modified:   package-lock.json 	modified:   package.json 	modified:   src/Pages/HomePage.jsx

--- src/Pages/HomePage.jsx
+++ src/Pages/HomePage.jsx
@@ -1,5 +1,7 @@
 import React from "react";
+import { Link, Element } from "react-scroll";
 import HomeComponent from "../Components/Home/HomeComponent";
+import AboutComponent from "../Components/About/AboutComponent";
 
 const HomePage = () => {
   return (
