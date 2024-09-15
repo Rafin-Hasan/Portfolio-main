8513d6eaf0b2629061801ba90d43937937b7fdf5
Rafin Hasan <168470414+Rafin-Hasan@users.noreply.github.com>
2024-09-15 23:48:44 +0600

    modified:   src/Pages/HomePage.jsx

--- src/Pages/HomePage.jsx
+++ src/Pages/HomePage.jsx
@@ -1,7 +1,5 @@
 import React from "react";
-import { Link, Element } from "react-scroll";
 import HomeComponent from "../Components/Home/HomeComponent";
-import AboutComponent from "../Components/About/AboutComponent";
 
 const HomePage = () => {
   return (
